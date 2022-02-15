0966f672166f221539f2e1680c2be0a04081d557
Qlaudyna992 <qlaudyna992@gmail.com>
2022-02-15 20:55:14 +0100

    First Commit

---
+++ NicolaZ.cpp
@@ -0,0 +1,36 @@
+﻿// zadania 6.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+ int potegi(int n, int m) {
+
+	return pow(n, m);
+
+}
+//Usun�am przerwy,kt�e tu by�y
+int main()
+{
+
+	int a, b;
+
+	cout << "Podaj LICZBE 1";
+	cin >> a;
+	cout << "Podaj LICZBE 2";
+	cin >> b;
+
+	if (a == 0 || b == 0) {
+
+		cout << "LICZBY muszą być różne od 0!!";
+
+	}
+	else {
+
+		cout << "LICZBA " << a << " do potegi " << b << " to: " << potegi(a, b);
+	}
+
+
+}
+
